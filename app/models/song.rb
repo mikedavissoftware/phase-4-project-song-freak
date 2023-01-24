846bad2d3873031693af1cfcc826f1b00e1c447e
Michael Davis <micdavis93@gmail.com>
2023-01-24 13:51:36 -0700

    back end done, merge

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,8 @@ class Song < ApplicationRecord
   has_many :likes
   has_many :users, through: :likes
 
-validates :title, presence: true
-validates :artist, presence: true
-validates :genre, presence: true
-validates :link, presence: true, inclusion: { in: %w(https://open.spotify.com/track/)}
+  validates :title, presence: true
+  validates :artist, presence: true
+  validates :genre, presence: true
+  validates :link, presence: true, inclusion: { in: %w(https://open.spotify.com/track/)}
 end
