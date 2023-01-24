340f1dd0d64d8cc2f228b336a6c2616ac1e16b30
Michael Davis <micdavis93@gmail.com>
2023-01-23 22:47:31 -0700

    got it fully working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     #May not want this when you introduce Auth, keep an eye on it
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
     def index
         users = User.all
         render json: users
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.permit(:name, :age, :bio, :image, :fav_genre, :fav_song)
+        params.permit(:name, :age, :bio, :image, :fav_genre, :fav_song_id)
     end
 end
--- app/serializers/user_with_liked_songs_serializer.rb
+++ app/serializers/user_with_liked_songs_serializer.rb
@@ -1,4 +1,4 @@
 class UserWithLikedSongsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :bio, :image, :fav_genre, :fav_song
+  attributes :id, :name, :age, :bio, :image, :fav_genre, :fav_song_id
   has_many :songs
 end
