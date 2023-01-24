beb7a9a11a7f773d8aa03afb57a131b2889f7c59
Michael Davis <micdavis93@gmail.com>
2023-01-23 22:25:37 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
-  resources :likes
-  resources :songs
+  resources :likes, only: [:create, :update, :destroy]
+  resources :songs, only: [:index, :show, :create, :update]
   resources :users
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
