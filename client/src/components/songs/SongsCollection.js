7e1229091baf8507bdbda351283b4b8199846e4b
Michael Davis <micdavis93@gmail.com>
2023-01-25 15:08:11 -0700

    added search to songs

--- client/src/components/songs/SongsCollection.js
+++ client/src/components/songs/SongsCollection.js
@@ -8,6 +8,7 @@ import SongCard from "../songs/SongCard"
 export default function SongsCollection() {
 
   const [songs, setSongs] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
 
   useEffect(() => {
     fetch("/songs")
@@ -17,13 +18,27 @@ export default function SongsCollection() {
     })
   }, [])
 
+  const filteredGamers = gamers.filter(gamer => {
+    return (
+      gamer.name.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      || Number(gamer.age).toString().includes(searchQuery)
+      || gamer.origin.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      || gamer.gender.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      || gamer.favorite.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      || gamer.bio.toString().toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  })
+
   const mappedSongs = songs.map(song => {
     return <SongCard key={song.id} song={song} />
   })
 
   return (
+    <>
+    <input></input>
     <Item.Group >
       {mappedSongs}
     </Item.Group>
+    </>
   )
 }
