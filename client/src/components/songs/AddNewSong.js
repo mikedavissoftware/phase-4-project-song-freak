259b9cedb9fd64a172b02e3a1afd5f697bd0fad0
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2023-01-26 16:05:09 -0700

    dropdown

--- client/src/components/songs/AddNewSong.js
+++ client/src/components/songs/AddNewSong.js
@@ -9,6 +9,23 @@ import {useState} from "react"
         link: "",
     })
 
+    const genres = [
+      "Rock",
+      "Hip Hop",
+      "Electronic",
+      "Metal",
+      "Pop",
+      "Classical",
+      "Country",
+      "Experimental",
+      "R&B",
+      "Jazz"
+    ]
+
+    const genreOptions = genres.map((genre) => {
+      return <option value={genre}>{genre}</option>
+    })
+
     function handleSubmit(e) {
       e.preventDefault();
 
@@ -26,14 +43,13 @@ import {useState} from "react"
       },
         body: JSON.stringify(newSong),
     })
-      .then ((r) => r.json())
-      .then ((newSong) => onAddNewSong(newSong))
+      .then (
       setFormData({
         title: "",
         artist: "",
         genre: "",
         link: "",
-    })
+    }))
 }
 
 function handleChange(event) {
@@ -46,15 +62,22 @@ function handleChange(event) {
         <div className="new-shoe-form">
             <h2>Post Your Favorite Songs</h2>
             
+          
+            
             <form onSubmit={handleSubmit}>
                 <input type="text" name="title" placeholder="Song Title" onChange={handleChange} value={formData.title} />
                 <input type="text" name="artist" placeholder="Artist Name" onChange={handleChange} value={formData.artist} />
-                <input type="text" name="genre" placeholder="Genre" onChange={handleChange} value={formData.genre} />
+                {/* <input type="text" name="genre" placeholder="Genre" onChange={handleChange} value={formData.genre} /> */}
+                
+                <select id="genre" name="genre" onChange={handleChange}>
+                  <option value={["unspecified"]} disabled selected>Select Genre...</option>
+                  {genreOptions}
+                </select>
                 <input type="text" name="link" placeholder="Spotify Song Link" onChange={handleChange} value={formData.link} />
                 <button type="submit">Post Song</button>
-
             </form>
         </div>
 
     );
 }
+
